a50bfb961df90f80654234a5cbb5b986982968c4
damienbod <damien_bod@hotmail.com>
2016-07-13 21:04:33 +0200

    init

---
+++ src/AspNetCoreInjectConfigurationRazor/Configuration/ApplicationConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreInjectConfigurationRazor.Configuration
+{
+    public class ApplicationConfigurations
+    {
+        public string ApplicationHostUrl { get; set; }
+
+        public string RestServiceTwo { get; set; }
+    }
+}
---
+++ src/AspNetCoreInjectConfigurationRazor/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreInjectConfigurationRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
