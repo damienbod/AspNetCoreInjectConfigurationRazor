e2d856284a4cf2db5a0e59edf6fb28f10ed2746b
damienbod <damien_bod@hotmail.com>
2017-02-03 20:05:54 +0100

    updating to VS2017 RC3 msbuild3

--- src/AspNetCoreInjectConfigurationRazor/Startup.cs
+++ src/AspNetCoreInjectConfigurationRazor/Startup.cs
@@ -41,7 +41,6 @@ namespace AspNetCoreInjectConfigurationRazor
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
